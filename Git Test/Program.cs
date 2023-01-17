4675fdb4adab75bf68dc776a8234d730f7d960c4
curjlync <curjlync@EPSG1-825043>
2023-01-17 13:36:44 -0600

    Add project files.

---
+++ Git Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Test
+{
+    class Program
+    {
+        private int steve;
+        public int getSteve()
+        {
+            return steve;
+        }
+        public void setSteve(int i)
+        {
+            steve = i;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World" + 5);
+            Console.ReadKey();
+            Console.WriteLine("Setting steve to 4");
+            Program p = new Program();
+            p.setSteve(4);
+            Console.WriteLine("Steve is: " + p.getSteve());
+            Console.ReadKey();
+        }
+    }
+}
