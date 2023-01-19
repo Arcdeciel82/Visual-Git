27e3d6e5e9a282d0b5afa99e4ab47693b8021ac5
curjlync <curjlync@ut.utm.edu>
2023-01-19 13:53:20 -0600

    file comment

--- Git Test/Program.cs
+++ Git Test/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// Curt Lynch
+// 1/19/2023
+// Getting into git
+// Hello world, with a simple number guessing game.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
